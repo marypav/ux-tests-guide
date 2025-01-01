18ecaf299b24d66fce5ad26b1c0f173d5100d80d
Mary <pavlenkompq@gmail.com>
2025-01-01 23:00:46 +0300

    Resolve merge conflicts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,63 +1,39 @@
-
-
 import {themes as prismThemes} from 'prism-react-renderer';
 
-const config = {
-  title: 'UX Tests Handbook',
-  tagline: 'Dinosaurs are cool',
+module.exports = {
   favicon: 'img/favicon.ico',
-  url: 'https://marypav.github.io/ux-tests-guide/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
+  title: 'UX Testing Guidelines',
+  url: 'https://marypav.github.io', // Только домен
+  baseUrl: '/',            // Название репозитория (или подкаталога)
+  projectName: 'ux-tests-guide',          // Название репозитория
+  organizationName: 'marypav',      // Ваше имя пользователя на GitHub
   presets: [
     [
-      'classic',
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          sidebarCollapsed: false,
-          routeBasePath: '/',
-          breadcrumbs: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://marypav.github.io/ux-tests-guide/',
-        },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      colorMode: {
-      respectPrefersColorScheme: true,
-    },
-    metadata: [
+      '@docusaurus/preset-classic',
       {
-        name: 'keywords',
-        content:
-          'UX Interview Handbook, UX Interview Questions, UX Interview Answers, UX Interview Preparation, UX research questions, UX design behavioral Interview Questions, Design Exercise Interview, UX Whiteboard Challenge Examples, User, Experience, Design, Research, Product, Interview, Preparation, Questions, Answers, design exercise interview',
+        docs: {
+          routeBasePath: '/', // Устанавливает документацию как корневой путь
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false, // Отключение блога (если не нужен)
       },
     ],
-  }),
+  ],
+  themeConfig: {
+    hideNavbar: false, 
+    navbar: {
+      title: 'UX Testing Guidelines',
+      logo: {
+        alt: 'Logo',
+        src: 'img/favicon.ico',
+      },
+    },
+  },
 };
 
 export default {
   title: 'UX Testing Guidelines',
-  url: 'https://marypav.github.io/ux-tests-guide/',
+  url: 'https://marypav.github.io',
   baseUrl: '/ux-tests-guide/',
   projectName: 'ux-tests-guide',
   favicon: '/img/favicon.png',
@@ -95,36 +71,16 @@ export default {
       copyright: `Copyright © 2023 UX Testing Guidelines. Built with <a href="https://docusaurus.io">Docusaurus</a>, open source, and free to use, forever.`, // You can also put own HTML here
     },
   },
-
-  themeConfig: {
-    docs: {
-      sidebar: {
-        hideable: true,
-      },
-    },
-    navbar: {
-      title: 'UX Testing Guidelines',
-      logo: {
-        alt: 'Site Logo',
-        src: '/img/favicon.ico',
-        width: 32,
-        height: 32,
-      },
-    },
-    footer: {
-      style: 'light',
-      copyright: `Copyright © 2023 UX Testing Guidelines. Built with <a href="https://docusaurus.io">Docusaurus</a>, open source, and free to use, forever.`, // You can also put own HTML here
-    },
-  },
   
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
-          routeBasePath: '/',
+          routeBasePath: '/', // Базовый путь для документации
+          sidebarPath: require.resolve('./sidebars.js'),
         },
-        blog: false,
+        blog: false, // так как блог не нужен
       },
     ],
   ],
